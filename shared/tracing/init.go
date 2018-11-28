0187f52590f1a08af7cdf44cc47aa748f2c43cf7
Joeri Vrijaldenhoven <joeri.vrijaldenhoven@ictu.nl>
2018-11-28 18:05:06 +0100

    changed init back

--- shared/tracing/init.go
+++ shared/tracing/init.go
@@ -16,13 +16,14 @@ func Init(service string, host string) (opentracing.Tracer, io.Closer) {
 //		// parsing errors might happen here, such as when we get a string where we expect a number
 //		log.Printf("Could not parse Jaeger env vars: %s", err.Error())
 //	}
+	println(host)
 	cfg := &config.Configuration{
 		Sampler: &config.SamplerConfig{
 			Type:  "const",
 			Param: 1,
 		},
 		Reporter: &config.ReporterConfig{
-			LocalAgentHostPort: host,
+			//LocalAgentHostPort: host,
 			LogSpans: true,
 		},
 	}
